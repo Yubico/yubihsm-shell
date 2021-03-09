cccf5234294d3da606d0513be4cfed657ce28d99
Per Nilsson <per.nilsson@yubico.com>
2021-03-09 16:59:08 +0100

    Implement ecdh primitives using bcrypt

--- common/ecdh.c
+++ common/ecdh.c
@@ -34,22 +34,138 @@
 
 #ifdef _WIN32_BCRYPT
 
-// TODO
+static const uint8_t n_P256[] = "\xff\xff\xff\xff\x00\x00\x00\x00"
+                                "\xff\xff\xff\xff\xff\xff\xff\xff"
+                                "\xbc\xe6\xfa\xad\xa7\x17\x9e\x84"
+                                "\xf3\xb9\xca\xc2\xfc\x63\x25\x51";
 
-int ecdh_curve_p256(void) { return 0; }
+static const BCRYPT_ALG_HANDLE curves[] = {NULL, BCRYPT_ECDH_P256_ALG_HANDLE};
+static const ULONG lengths[] = {0, 256};
+
+int ecdh_curve_p256(void) { return 1; }
+
+static int bn_cmp(const uint8_t *a, const uint8_t *b, size_t cb) {
+  for (size_t i = 0; i < cb; i++) {
+    if (a[i] < b[i])
+      return -1;
+    if (a[i] > b[i])
+      return 1;
+  }
+  return 0;
+}
+
+static int validate_privkey(int curve, const uint8_t *privkey,
+                            size_t cb_privkey) {
+  return curve == 1 && cb_privkey == 32 && bn_cmp(privkey, n_P256, 32) < 0;
+}
+
 int ecdh_calculate_public_key(int curve, const uint8_t *privkey,
                               size_t cb_privkey, uint8_t *pubkey,
                               size_t cb_pubkey) {
-  return 0;
+  int rc = 0;
+  if (validate_privkey(curve, privkey, cb_privkey)) {
+    uint8_t buf[256];
+    BCRYPT_ECCKEY_BLOB *blob = (BCRYPT_ECCKEY_BLOB *) buf;
+    blob->dwMagic = BCRYPT_ECDH_PRIVATE_P256_MAGIC;
+    blob->cbKey = cb_privkey;
+    memset(buf + sizeof(BCRYPT_ECCKEY_BLOB), 0, 2 * cb_privkey);
+    memcpy(buf + sizeof(BCRYPT_ECCKEY_BLOB) + 2 * cb_privkey, privkey,
+           cb_privkey);
+    BCRYPT_KEY_HANDLE key;
+    NTSTATUS status =
+      BCryptImportKeyPair(curves[curve], NULL, BCRYPT_ECCPRIVATE_BLOB, &key,
+                          buf, sizeof(BCRYPT_ECCKEY_BLOB) + 3 * cb_privkey,
+                          BCRYPT_NO_KEY_VALIDATION);
+    if (BCRYPT_SUCCESS(status)) {
+      ULONG cb;
+      status = BCryptExportKey(key, NULL, BCRYPT_ECCPUBLIC_BLOB, buf,
+                               sizeof(buf), &cb, 0);
+      if (BCRYPT_SUCCESS(status) && cb_pubkey > 2 * blob->cbKey) {
+        *pubkey = 4;
+        memcpy(pubkey + 1, buf + sizeof(BCRYPT_ECCKEY_BLOB), 2 * blob->cbKey);
+        rc = 1 + 2 * blob->cbKey;
+      }
+      BCryptDestroyKey(key);
+    }
+  }
+  return rc;
 }
+
 int ecdh_generate_keypair(int curve, uint8_t *privkey, size_t cb_privkey,
                           uint8_t *pubkey, size_t cb_pubkey) {
-  return 0;
+  int rc = 0;
+  BCRYPT_KEY_HANDLE key;
+  NTSTATUS status =
+    BCryptGenerateKeyPair(curves[curve], &key, lengths[curve], 0);
+  if (BCRYPT_SUCCESS(status)) {
+    status = BCryptFinalizeKeyPair(key, 0);
+    if (BCRYPT_SUCCESS(status)) {
+      uint8_t buf[256];
+      ULONG cb;
+      status = BCryptExportKey(key, NULL, BCRYPT_ECCPRIVATE_BLOB, buf,
+                               sizeof(buf), &cb, 0);
+      BCRYPT_ECCKEY_BLOB *blob = (BCRYPT_ECCKEY_BLOB *) buf;
+      if (BCRYPT_SUCCESS(status) && cb_privkey >= blob->cbKey &&
+          cb_pubkey > 2 * blob->cbKey) {
+        *pubkey = 4;
+        memcpy(pubkey + 1, buf + sizeof(BCRYPT_ECCKEY_BLOB), 2 * blob->cbKey);
+        memcpy(privkey, buf + sizeof(BCRYPT_ECCKEY_BLOB) + 2 * blob->cbKey,
+               blob->cbKey);
+        rc = blob->cbKey;
+      }
+    }
+    BCryptDestroyKey(key);
+  }
+  return rc;
 }
+
 int ecdh_calculate_secret(int curve, const uint8_t *privkey, size_t cb_privkey,
                           const uint8_t *pubkey, size_t cb_pubkey,
                           uint8_t *secret, size_t cb_secret) {
-  return 0;
+  int rc = 0;
+  uint8_t buf[256];
+  BCRYPT_ECCKEY_BLOB *blob = (BCRYPT_ECCKEY_BLOB *) buf;
+  blob->dwMagic = BCRYPT_ECDH_PRIVATE_P256_MAGIC;
+  blob->cbKey = cb_privkey;
+  memset(buf + sizeof(BCRYPT_ECCKEY_BLOB), 0, 2 * cb_privkey);
+  memcpy(buf + sizeof(BCRYPT_ECCKEY_BLOB) + 2 * cb_privkey, privkey,
+         cb_privkey);
+  BCRYPT_KEY_HANDLE priv;
+  NTSTATUS status =
+    BCryptImportKeyPair(curves[curve], NULL, BCRYPT_ECCPRIVATE_BLOB, &priv, buf,
+                        sizeof(BCRYPT_ECCKEY_BLOB) + 3 * cb_privkey,
+                        BCRYPT_NO_KEY_VALIDATION);
+  if (BCRYPT_SUCCESS(status)) {
+    blob->dwMagic = BCRYPT_ECDH_PUBLIC_P256_MAGIC;
+    blob->cbKey = cb_privkey;
+    memcpy(buf + sizeof(BCRYPT_ECCKEY_BLOB), pubkey + 1, cb_pubkey - 1);
+    BCRYPT_KEY_HANDLE pub;
+    status =
+      BCryptImportKeyPair(curves[curve], NULL, BCRYPT_ECCPUBLIC_BLOB, &pub, buf,
+                          sizeof(BCRYPT_ECCKEY_BLOB) + 2 * cb_privkey, 0);
+    if (BCRYPT_SUCCESS(status)) {
+      BCRYPT_SECRET_HANDLE sec;
+      status = BCryptSecretAgreement(priv, pub, &sec, 0);
+      if (BCRYPT_SUCCESS(status)) {
+        ULONG cb;
+        status = BCryptDeriveKey(sec, BCRYPT_KDF_RAW_SECRET, NULL, secret,
+                                 cb_secret, &cb, 0);
+        if (BCRYPT_SUCCESS(status)) {
+          // BCRYPT_KDF_RAW_SECRET returns little-endian so reverse the array
+          for (ULONG c = 0; c < cb / 2; c++) {
+            uint8_t t = secret[c];
+            secret[c] = secret[cb - c - 1];
+            secret[cb - c - 1] = t;
+          }
+          rc = cb;
+        }
+        BCryptDestroySecret(sec);
+      }
+      BCryptDestroyKey(pub);
+    }
+    BCryptDestroyKey(priv);
+  }
+  return rc;
 }
 
 #else
