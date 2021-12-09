9bacc8d58e77ce0c7ce87212bb80d9d412613255
Per Nilsson <per.nilsson@yubico.com>
2021-12-09 15:07:41 +0100

    Tuned cmakefiles

--- cmake/SecurityFlags.cmake
+++ cmake/SecurityFlags.cmake
@@ -1,24 +1,23 @@
-include(CheckCCompilerFlag)
-if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-    include(CheckLinkerFlag)
-endif()
-
 set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 
 if (CMAKE_C_COMPILER_ID STREQUAL "Clang" OR
     CMAKE_C_COMPILER_ID STREQUAL "AppleClang" OR
     CMAKE_C_COMPILER_ID STREQUAL "GNU")
 
+    include(CheckCCompilerFlag)
+    check_c_compiler_flag("-Wshorten-64-to-32" HAVE_SHORTEN_64_TO_32)
     check_c_compiler_flag("-fstack-protector-all" HAVE_STACK_PROTECTOR_ALL)
-    if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+
+    if (CMAKE_VERSION VERSION_GREATER 3.18)
+        include(CheckLinkerFlag)
         check_linker_flag(C "-Wl,-z,relro,-z,now" HAVE_RELRO)
         check_linker_flag(C "-Wl,-z,noexecstack" HAVE_NOEXECSTACK)
-    endif()
+    endif ()
 
     add_compile_options (-Wall -Wextra -Werror)
     add_compile_options (-Wformat -Wformat-nonliteral -Wformat-security)
     add_compile_options (-Wshadow)
-    add_compile_options (-Wcast-qual)
+    #add_compile_options (-Wcast-qual)
     add_compile_options (-Wmissing-prototypes)
     add_compile_options (-Wbad-function-cast)
     add_compile_options (-Wno-implicit-fallthrough)
@@ -29,16 +28,22 @@ if (CMAKE_C_COMPILER_ID STREQUAL "Clang" OR
         add_definitions (-D_FORTIFY_SOURCE=2)
     endif ()
 
-	if(HAVE_STACK_PROTECTOR_ALL)
-        message(STATUS "-fstack-protector-all support detected")
+    #set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -g2")
+    #set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -fno-omit-frame-pointer")
+
+    if(HAVE_SHORTEN_64_TO_32)
+        #add_compile_options (-Wshorten-64-to-32)
+    endif()
+
+    if(HAVE_STACK_PROTECTOR_ALL)
 		add_compile_options(-fstack-protector-all)
 	endif()
+
     if (HAVE_RELRO)
-        message(STATUS "relro support detected")
         add_link_options(-Wl,-z,relro,-z,now)
     endif ()
+
     if (HAVE_NOEXECSTACK)
-        message(STATUS "noexecstack support detected")
         add_link_options (-Wl,-z,noexecstack)
     endif ()
 elseif (CMAKE_C_COMPILER_ID STREQUAL "MSVC")
