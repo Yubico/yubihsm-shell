861c53421d688112eb12166078ddf6900f3db230
Aveen Ismail <aveen.ismail@yubico.com>
2023-02-27 11:35:51 +0100

    GithubActions: Remove sudo from commands run in container

--- resources/release/linux/build-rpm-in-githunactions.sh
+++ resources/release/linux/build-rpm-in-githunactions.sh
@@ -5,9 +5,9 @@ set -x
 PLATFORM=$1
 
 if [ "$PLATFORM" == "centos7" ]; then
-  sudo yum -y install centos-release-scl
-  sudo yum -y update && sudo yum -y upgrade
-  sudo yum -y install devtoolset-7-gcc     \
+  yum -y install centos-release-scl
+  yum -y update && yum -y upgrade
+  yum -y install devtoolset-7-gcc     \
                       devtoolset-7-gcc-c++ \
                       devtoolset-7-make    \
                       chrpath              \
@@ -25,18 +25,18 @@ if [ "$PLATFORM" == "centos7" ]; then
 
   GENGETOPT_VER=2.23
   curl -o gengetopt-${GENGETOPT_VER}.rpm https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/g/gengetopt-2.23-1.el7.x86_64.rpm
-  sudo yum -y install ./gengetopt-${GENGETOPT_VER}.rpm
+  yum -y install ./gengetopt-${GENGETOPT_VER}.rpm
 
   export CMAKE="cmake"
 
 elif [ "$PLATFORM" == "centos8" ]; then
-  sudo yum -y install epel-release
-  sudo yum -y update && sudo yum -y upgrade
+  yum -y install epel-release
+  yum -y update && yum -y upgrade
 
-  sudo dnf group -y install "Development Tools"
-  sudo dnf config-manager -y --set-enabled powertools
+  dnf group -y install "Development Tools"
+  dnf config-manager -y --set-enabled powertools
 
-sudo yum -y install chrpath              \
+yum -y install chrpath              \
                     git                  \
                     cmake3               \
                     gengetopt            \
@@ -49,8 +49,8 @@ sudo yum -y install chrpath              \
   export CMAKE="cmake3"
 
 elif [ "${PLATFORM:0:6}" == "fedora" ]; then
-  sudo dnf -y update
-  sudo dnf -y install binutils         \
+  dnf -y update
+  dnf -y install binutils         \
                       git              \
                       chrpath          \
                       cmake            \
