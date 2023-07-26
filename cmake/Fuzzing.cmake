87234f357a7831d60dd6a83b26d3aab8a1bbe48e
Alexandru Geana <alex.geana@yubico.com>
2023-07-26 18:12:44 +0200

    Fuzzing: enable ASAN or MSAN fuzzing in cmake

--- cmake/Fuzzing.cmake
+++ cmake/Fuzzing.cmake
@@ -1,21 +1,27 @@
 option(FUZZING "Compile binaries with fuzzing instrumentation" OFF)
 option(LIBFUZZER_ASAN "Enable ASAN instrumentation with libfuzzer" OFF)
+option(FUZZING_MSAN "Compile binaries with MemorySanitizer instrumentation" OFF)
 
 if (FUZZING)
     message(STATUS "Building with fuzzing instrumentation.")
 
     string (APPEND CMAKE_C_FLAGS " -DFUZZING")
-    string (APPEND CMAKE_C_FLAGS " -fno-omit-frame-pointer -O1")
+    string (APPEND CMAKE_C_FLAGS " -fno-omit-frame-pointer -O1 -g")
 
     string (APPEND CMAKE_CXX_FLAGS " -std=c++17")
     string (APPEND CMAKE_CXX_FLAGS " -DFUZZING")
-    string (APPEND CMAKE_CXX_FLAGS " -fno-omit-frame-pointer -O1")
+    string (APPEND CMAKE_CXX_FLAGS " -fno-omit-frame-pointer -O1 -g")
 
-    string (APPEND CMAKE_C_FLAGS " -fsanitize=address")
-    string (APPEND CMAKE_CXX_FLAGS " -fsanitize=address")
-    string (APPEND CMAKE_EXE_LINKER_FLAGS " -fsanitize=address")
+    string (APPEND CMAKE_EXE_LINKER_FLAGS " -g")
 
-    string (APPEND CMAKE_C_FLAGS " -fsanitize=undefined")
-    string (APPEND CMAKE_CXX_FLAGS " -fsanitize=undefined")
-    string (APPEND CMAKE_EXE_LINKER_FLAGS " -fsanitize=undefined")
+    if (FUZZING_MSAN)
+        string (APPEND CMAKE_C_FLAGS " -fsanitize=memory")
+        string (APPEND CMAKE_CXX_FLAGS " -fsanitize=memory")
+        string (APPEND CMAKE_EXE_LINKER_FLAGS " -fsanitize=memory")
+    else (FUZZING_MSAN)
+        string (APPEND CMAKE_C_FLAGS " -fsanitize=address -fsanitize=undefined")
+        string (APPEND CMAKE_CXX_FLAGS " -fsanitize=address -fsanitize=undefined")
+        string (APPEND CMAKE_EXE_LINKER_FLAGS " -fsanitize=address -fsanitize=undefined")
+    endif (FUZZING_MSAN)
+
 endif ()
