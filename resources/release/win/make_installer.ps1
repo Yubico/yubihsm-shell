ef0048f3a4b9546e571220401d9480f6480f8490
Aveen Ismail <42414844+aveenismail@users.noreply.github.com>
2026-07-01 15:34:18 +0200

    Update release scripts (#535)

--- resources/release/win/make_installer.ps1
+++ resources/release/win/make_installer.ps1
@@ -1,12 +1,13 @@
 if($args.length -lt 3)
 {
-    echo "Usage: ./repack_installer.ps1 <x86|x64> <WIX_PATH> <MERGE_MODULE_PATH> [<SIGNED_BINARIES_PATH>]"
+    echo "Usage: ./make_installer.ps1 <x86|x64> <VERSION> <WIX_PATH> <MERGE_MODULE_PATH> [<SIGNED_BINARIES_PATH>]"
     echo ""
     echo "This is a script to build an MSI installer for yubihsm"
     echo ""
     echo "   x86                    builds the installer for X86 architecture"
     echo "   x64                    builds the installer for X64 architecture"
     echo ""
+    echo "   VERSION                Release version, tex 2.6.3"
     echo "   WIX_PATH               Absolute path to the directory where WIX Tools binaries (heat.exe, candle.exe and light.exe) are located"
     echo "   MERGE_MODULE_PATH      Absolute path to the redistribution module (tex Microsoft_VC142_CRT_x86.msm or Microsoft_VC142_CRT_x64.msm)"
     echo "   SIGNED_BINARIES_PATH   (Optional) Absolute path to signed binaries. If not spacified, YUBIHSM-SHELL/resources/release/win/yubihsm-shell-[x86|x64] is assumed"
@@ -14,15 +15,16 @@ if($args.length -lt 3)
 }
 
 $ARCH=$args[0]
-$WIX_PATH=$args[1] # Absolute path to the WixTools binaries
-$MERGE_MODULE=$args[2] # Absolute path containing Microsoft_VC142_CRT_x86.msm or Microsoft_VC142_CRT_x64.msm
+$VERSION=$args[1]
+$WIX_PATH=$args[2] # Absolute path to the WixTools binaries
+$MERGE_MODULE=$args[3] # Absolute path containing Microsoft_VC142_CRT_x86.msm or Microsoft_VC142_CRT_x64.msm
 
 $WIN_DIR = "$PSScriptRoot"
 $SOURCE_DIR="$PSScriptRoot/../../.."
 
-if($args.length -eq 4)
+if($args.length -eq 5)
 {
-    $RELEASE_DIR=$args[3]
+    $RELEASE_DIR=$args[4]
 }
 else
 {
@@ -39,12 +41,21 @@ $env:MERGEDPATH = $MERGE_MODULE
 
 heat.exe dir $RELEASE_DIR -out fragment.wxs -gg -scom -srd -sfrag -sreg -dr INSTALLDIR -cg ApplicationFiles -var env.SRCDIR
 candle.exe fragment.wxs "yubihsm-shell_$ARCH.wxs" -ext WixUtilExtension  -arch $ARCH
-light.exe fragment.wixobj "yubihsm-shell_$ARCH.wixobj" -ext WixUIExtension -ext WixUtilExtension -o "yubihsm-shell-$ARCH.msi"
+light.exe fragment.wixobj "yubihsm-shell_$ARCH.wixobj" -ext WixUIExtension -ext WixUtilExtension -o "yubihsm-shell-$VERSION-$ARCH.msi"
 
 #cleanup
 rm fragment.wxs
 rm fragment.wixobj
 rm "yubihsm-shell_$ARCH.wixobj"
-rm "yubihsm-shell-$ARCH.wixpdb"
+rm "yubihsm-shell-$VERSION-$ARCH.wixpdb"
+
+if($ARCH -eq "x86")
+{
+    mv "yubihsm-shell-$VERSION-$ARCH.msi" "yubihsm-shell-$VERSION-win32.msi"
+}
+else
+{
+    mv "yubihsm-shell-$VERSION-$ARCH.msi" "yubihsm-shell-$VERSION-win64.msi"
+}
 
 Set-PSDebug -Trace 0
